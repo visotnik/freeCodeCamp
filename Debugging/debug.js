65300b8452c631586abcd5faa6e2b701c279cb0e
visotnik <tet129@gmail.com>
2022-02-10 11:09:47 +0200

    debugging to be continued

--- Debugging/debug.js
+++ Debugging/debug.js
@@ -12,10 +12,19 @@ let sumAB = a + b;
 console.log(sumAB);
 
 // =================================================================================
-
+// Use console.log / warn / error / clear for debugging
 let output =
   "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output);
 console.clear();
+
+// =================================================================================
+// Use typeof() to check the data structure
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
 
 // =================================================================================
